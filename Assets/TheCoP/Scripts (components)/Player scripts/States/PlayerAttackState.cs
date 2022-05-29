087175495e80b07e4cb45431bb71f3880c07eb2a
KolyanPie <mbilibin@mail.ru>
2022-05-29 22:14:26 +0300

    add Player state machine and states

---
+++ Assets/TheCoP/Scripts (components)/Player scripts/States/PlayerAttackState.cs
@@ -0,0 +1,47 @@
+﻿using TheCoP.Architecture.State_Machines;
+
+using UnityEngine;
+
+namespace TheCoP.Scripts__components_.Player_scripts.States
+{
+    public class PlayerAttackState : IState
+    {
+        private readonly Player _player;
+        private const int StartAngle = -85;
+        private const float RotationAngle = 150f;
+        private const float RotationTime = 0.2f;
+        private const float Damage = 10;
+
+        public PlayerAttackState(Player player)
+        {
+            _player = player;
+        }
+
+        public void Tick()
+        {
+        }
+
+        public void FixTick()
+        {
+        }
+
+        public void OnEnter()
+        {
+            _player.PlayAnimator("Attack");
+            var hitBox = Object.Instantiate(
+                _player.Box,
+                _player.transform.position,
+                Quaternion.Euler(0, 0, _player.Direction - StartAngle),
+                _player.transform
+            );
+            var hitBoxComponent = hitBox.GetComponent<HitBox>();
+            hitBoxComponent.rotationAngle = RotationAngle;
+            hitBoxComponent.rotationTime = RotationTime;
+            hitBoxComponent.damage = Damage;
+        }
+
+        public void OnExit()
+        {
+        }
+    }
+}
---
+++ Assets/TheCoP/Scripts (components)/Player scripts/States/PlayerMovementState.cs
@@ -0,0 +1,53 @@
+﻿using TheCoP.Architecture.State_Machines;
+
+using UnityEngine;
+
+namespace TheCoP.Scripts__components_.Player_scripts.States
+{
+    public class PlayerMovementState : IState
+    {
+        private static readonly int Walk = Animator.StringToHash("Walk");
+        private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int Vertical = Animator.StringToHash("Vertical");
+
+        private readonly Player _player;
+        private readonly Animator _animator;
+        private readonly Rigidbody2D _rigidbody2D;
+
+        public PlayerMovementState(Player player, Animator animator, Rigidbody2D rigidbody2D)
+        {
+            _player = player;
+            _animator = animator;
+            _rigidbody2D = rigidbody2D;
+        }
+
+        public void Tick()
+        {
+            var isMoving = _player.Movement.x != 0 || _player.Movement.y != 0;
+            if (!isMoving) return;
+
+            _animator.SetBool(Walk, true);
+            _animator.SetFloat(Horizontal, _player.Movement.x);
+            _animator.SetFloat(Vertical, _player.Movement.y);
+            var angle = Vector2.Angle(_player.Movement, Vector2.up);
+
+            if (_player.Movement.x < 0)
+                _player.Direction = (360f - angle) * -1f;
+            else
+                _player.Direction = angle * -1f;
+        }
+
+        public void FixTick()
+        {
+            _rigidbody2D.velocity = _player.Movement * _player.Speed;
+        }
+
+        public void OnEnter()
+        {
+        }
+
+        public void OnExit()
+        {
+        }
+    }
+}
