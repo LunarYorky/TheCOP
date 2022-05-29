e0b35487053868441e9ee4260721daa64d2b718f
LunarYorky <lunaryorky@gmail.com>
2022-05-29 16:46:54 +0400

    Remake Camera Movement

---
+++ Scripts (components)/CameraMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject target;
+
+    private void LateUpdate()
+    {
+        Vector2 vector2 = Vector2.Lerp(gameObject.transform.position, target.transform.position, 0.05f);
+        gameObject.transform.position = new Vector3(vector2.x, vector2.y, -10);
+
+    }
+}
+ 
