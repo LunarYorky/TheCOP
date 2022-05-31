66a35d8f3d306dea8d2aecf83b1c5da30450fadc
LunarYorky <lunaryorky@gmail.com>
2022-05-31 14:24:10 +0400

    EnemiesData scriptable obj created

---
+++ Assets/TheCoP/Architecture/Scriptable Objects/EnemieData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine.Animations;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New EnemieData", menuName = "ScriptableObjects/EnemieData")]
+public class EnemieData : ScriptableObject
+{
+    [SerializeField] private string enemieName;
+    [SerializeField] private RuntimeAnimatorController animatorController;
+
+    public string EnemieName => enemieName;
+    public RuntimeAnimatorController AnimatorController => animatorController;
+}
